5128765120a60b7bd13e511015bf8f26519a6ace
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-01-19 12:57:50 -0500

    Added iOS project and Ryan's grouping prototype Java project "Mercury Reader."

---
+++ iOS/iPad/AppDelegate_iPad.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPad.h
+//  NewsBox
+//
+//  Created by Ryan Ashcraft on 1/16/11.
+//  Copyright 2011 Ashcraft Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPad : AppDelegate_Shared {
+}
+
+
+@end
+
---
+++ iOS/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPhone.h
+//  NewsBox
+//
+//  Created by Ryan Ashcraft on 1/16/11.
+//  Copyright 2011 Ashcraft Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared {
+}
+
+
+@end
+
