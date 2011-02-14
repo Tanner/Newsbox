e23a69862bfc8d47998167f0388615a5cc274a3f
Tanner Smith <tanner@tssoftware.net>
2011-02-14 15:30:13 -0500

    Remove setting latestGitSHA to nil and release it from viewDidUnload.

--- iOS/iPhone/SettingsTableViewController_iPhone.m
+++ iOS/iPhone/SettingsTableViewController_iPhone.m
@@ -271,8 +271,6 @@
 - (void)viewDidUnload {
     // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
     // For example: self.myOutlet = nil;
-    latestGitSHA = nil;
-    [latestGitSHA release];
 }
 
 
