d7ff383005adb9c74575179d6bf67a384073b9e9
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-05-16 22:12:23 -0400

    Add SourceTableViewCell for SourcesTableViewController. Using, TDBadgedCell, add unread count badge to SourceTableViewCell. Add unread Item fetch requests.

---
+++ iOS/iPhone/SourceTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SourceTableViewCell.h
+//  Newsbox
+//
+//  Created by Ryan Ashcraft on 5/16/11.
+//  Copyright 2011 Ashcraft Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TDBadgedCell.h"
+
+@interface SourceTableViewCell : TDBadgedCell {
+
+}
+
+@end
